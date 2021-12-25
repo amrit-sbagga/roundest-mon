b71767948e5707c78bd2f38f2e22576b89b2b7e4
“Amrit <amritsingh@Amrits-MacBook-Pro.local>
2021-12-26 02:22:45 +0530

    backend with pokemon data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 //import '../styles/global.css'
 import { trpc } from "@/utils/trpc";
 import { getOptionsForVote } from '@/utils/getRandomPokemon';
+import { useState } from 'react';
 
 const Home: NextPage = () => {
   // const {data , isLoading} = trpc.useQuery(["hello", {text: "Amrit"}]) //calling query name of backed->router
@@ -14,7 +15,16 @@ const Home: NextPage = () => {
   //   return <div>{data.greeting}</div>
   // }
 
-  const [first, second] = getOptionsForVote();
+  //const [first, second] = useMemo(() => getOptionsForVote(), []);
+  const [ids, updateIds] = useState(() => getOptionsForVote());
+  const [first, second] = ids;
+
+  const firstPokemon = trpc.useQuery(["get-pokemon-by-id", {id: first }]);
+  //console.log(firstPokemon.data);
+
+  const secondPokemon = trpc.useQuery(["get-pokemon-by-id", {id: second }]);
+  
+  if(firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center align-center-middle">
@@ -22,9 +32,16 @@ const Home: NextPage = () => {
       <div className="text-2xl text-center">Which pokemon is rounder?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-800">{first}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <img src={firstPokemon.data?.sprites.front_default!} className="w-full"/>
+          <div className='text-xl text-center capitalize mt-[-2rem]'>{firstPokemon.data?.name}</div>
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-800">{second}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <img src={secondPokemon.data?.sprites.front_default!} className="w-full"/>
+          <div className='text-xl text-center capitalize mt-[-2rem]'>{secondPokemon.data?.name}</div>
+        </div>
+        <div className='p-2'/>
       </div>
 
     </div>
